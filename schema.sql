4002c632692e45b016fd8600adac03bab0b7d04f
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-20 16:24:43 -0500

    Create database and table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
+
+\connect vet_clinic
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(30),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered boolean,
+    weight_kg decimal,
+    PRIMARY KEY (id)
 );
