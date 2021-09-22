58e2b43bb8fa10b10b8699fc7661dbbf29ffe4e1
ArthurGC <alxguzmanc@gmail.com>
2021-09-22 17:20:21 -0500

    Add new data to owners table

--- data.sql
+++ data.sql
@@ -21,5 +21,20 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 	VALUES ('Angemon', '2005-06-12', 1, TRUE, -45);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 	VALUES ('Boarmon', '2005-06-7', 7, TRUE, 20.4);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)++
 	VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
+
+/* Data for third step*/
+
+INSERT INTO owners (full_name, age)
+	VALUES('Sam Smith', 34);
+INSERT INTO owners (full_name, age)
+	VALUES('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age)
+	VALUES('Bob', 45);
+INSERT INTO owners (full_name, age)
+	VALUES('Melody Pond', 77);
+INSERT INTO owners (full_name, age)
+	VALUES('Dean Winchester', 14);
+INSERT INTO owners (full_name, age)
+	VALUES('Jodie Whittaker', 38);
