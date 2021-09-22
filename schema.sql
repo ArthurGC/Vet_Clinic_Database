d7dbcd82196c6de2bcad3683bf5edd93c5348a72
ArthurGC <alxguzmanc@gmail.com>
2021-09-22 17:13:51 -0500

    Edit animals table to add foreign keys

--- schema.sql
+++ schema.sql
@@ -30,3 +30,23 @@ CREATE TABLE species (
     name VARCHAR(30),
     PRIMARY KEY (id)
 )
+
+/*Modify animals table*/
+/*Make sure id is autoincremented primary key*/
+
+ALTER TABLE animals 
+    DROP COLUMN species;
+ALTER TABLE animals
+    ADD COLUMN species_id INT;
+ALTER TABLE animals
+    ADD COLUMN owner_id INT;
+ALTER TABLE animals 
+    ADD CONSTRAINT fk_species
+        FOREIGN KEY (species_id) 
+            REFERENCES species (id);
+            ON DELETE CASCADE
+ALTER TABLE animals 
+    ADD CONSTRAINT fk_owner
+        FOREIGN KEY (owner_id) 
+            REFERENCES owners (id);
+            ON DELETE CASCADE
