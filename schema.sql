22bbec1e6a42cf10ea68eb79976b94fa3abb456a
ArthurGC <alxguzmanc@gmail.com>
2021-09-22 13:34:44 -0500

    Create owners and species tables

--- schema.sql
+++ schema.sql
@@ -12,6 +12,21 @@ CREATE TABLE animals (
     weight_kg decimal,
     PRIMARY KEY (id)
 );
-
+/*Add species column*/
 ALTER TABLE animals
     ADD COLUMN species VARCHAR(30);
+
+/*Create owners table*/
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(30),
+    age INT,
+    PRIMARY KEY (id)
+)
+
+/*Create species table*/
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+)
